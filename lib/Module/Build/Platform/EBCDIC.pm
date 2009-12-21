091f9c19cd18b1dc217ca481311353ce2857736f
David Golden <dagolden@cpan.org>
2009-12-21 19:48:12 +0000

    Import of DAGOLDEN/Module-Build-0.3601 from CPAN.
    gitpan-cpan-distribution: Module-Build
    gitpan-cpan-version:      0.3601
    gitpan-cpan-path:         DAGOLDEN/Module-Build-0.3601.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Module/Build/Platform/EBCDIC.pm
+++ lib/Module/Build/Platform/EBCDIC.pm
@@ -2,7 +2,7 @@ package Module::Build::Platform::EBCDIC;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.36';
+$VERSION = '0.3601';
 $VERSION = eval $VERSION;
 use Module::Build::Base;
 
--- lib/Module/Build/Platform/Windows.pm
+++ lib/Module/Build/Platform/Windows.pm
@@ -2,7 +2,7 @@ package Module::Build::Platform::Windows;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.36';
+$VERSION = '0.3601';
 $VERSION = eval $VERSION;
 
 use Config;
